6ab8af00c68964bd0194db7b0cd52a0e02600969
YairMoller <yairmoller@hilma.tech>
2024-09-30 16:11:06 +0300

    more game functions

--- js/basic-game-functions.js
+++ js/basic-game-functions.js
@@ -5,41 +5,69 @@ function Entity(elemnt) {
         (this.height = parseInt(getComputedStyle(document.getElementById(elemnt)).getPropertyValue("height")));
 }
 
-const container = new Entity("container");
-console.log(container);
+const player = new Entity("player");
+console.log(player);
+const player2 = new Entity("player2");
 
-const moveRightLeft = (entity, direction, amount = 0) => {
-    let movingEntity = document.getElementById("container");
+const move = (entity, direction, amount = 0) => {
+    let movingEntity = document.getElementById("player");
+    console.log("movingEntity: ", movingEntity);
     if (direction == "right") {
         entity.x += amount;
         movingEntity.style.left = entity.x + "px";
-    } else {
+    } else if (direction == "left") {
         entity.x -= amount;
         movingEntity.style.left = entity.x + "px";
+    } else if (direction == "up") {
+        entity.y -= amount;
+        movingEntity.style.left = entity.y + "px";
     }
 };
 
-const notMoving = setInterval(moveRightLeft(container, "right"), 5);
+let keysPressed = {};
 
-document.onkeydown = function () {
-    switch (event.key) {
-        case "ArrowRight":
-            moveRightLeft("container", "right", 1);
-            break;
-        case "ArrowLeft":
-            moveRightLeft("container", "left", 1);
+document.addEventListener("keydown", (event) => {
+    keysPressed[event.key] = true;
+    console.log("keysPressed[event.key]: ", event.key);
+});
+document.addEventListener("keyup", (event) => (keysPressed[event.key] = false));
+
+const detectMovment = () => {
+    if (keysPressed["ArrowRight"] == true) {
+        move(player, "right", 1);
+    } else if (keysPressed["ArrowLeft"] == true) {
+        move(player, "left", 1);
+    }
+    if (keysPressed["ArrowUP"] == true) {
+        jump();
     }
 };
+
+const gravity = -9.8; //todo upper
+const startingVelocity = 44.2718872;
+let velocity = startingVelocity;
+
+const caculateJump = (velocity) => {
+    move(player, "up", velocity);
+    velocity += gravity / 20;
+    if (touching(player, player2 || player.y == 0)) {
+        clearInterval(JumpTime);
+        velocity = startingVelocity;
+    }
+};
+
+const jump = () => {
+    if (touching(player, player2 || player.y == 0)) {
+        const JumpTime = setInterval(() => caculateJump(velocity), 50);
+    }
+};
+
+const constantChecking = setInterval(detectMovment, 50);
 
 const touching = (entity1, entity2) => {
-    const obj1 = new Entity(entity1);
-    const obj2 = new Entity(entity2);
-
-    if (obj1.x >= obj2.x && obj1.x <= obj2.x + obj2.width && obj1.y + obj1.height >= obj1.y + obj2.height) {
+    if (entity1.x >= entity2.x && entity1.x <= entity2.x + entity2.width && entity1.y + entity1.height >= entity1.y + entity2.height) {
         return true;
     } else {
         return false;
     }
 };
-
-console.log(touching("container", "container2"));
