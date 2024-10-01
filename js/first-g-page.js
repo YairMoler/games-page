1c320bbea0f5272f7c562525efea2b52ef442924
tair18oz <tairoz@hilma.tech>
2024-10-01 09:26:14 +0300

    delate

--- js/first-g-page.js
+++ js/first-g-page.js
@@ -21,3 +21,42 @@ playerDiv = document.createElement("div");
 playerDiv.setAttribute("id", "player");
 playerDiv.setAttribute("style", `left: 0px; bottom: ${wall[0].height}px;`);
 element.appendChild(playerDiv);
+
+
+/* <body onload="startGame()">
+*/
+/*var myGamePiece;
+var myObstacles = [];
+var mySound;
+var myMusic;
+
+function startGame() {
+  myGamePiece = new component(30, 30, "red", 10, 120);
+  mySound = new sound("bounce.mp3");
+  myMusic = new sound("gametheme.mp3");
+  myMusic.play();
+  myGameArea.start();
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
